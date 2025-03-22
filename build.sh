a12eb18962b6781668200a04ca260f788df4ad53
VXNOM12 <harshkatalkar67@gmail.com>
2025-03-22 12:10:51 +0530

    added BUILD FILE

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ echo "Installing system dependencies for scientific Python packages..."
 apt-get update -y
 
 # Install essential build tools and scientific computing dependencies
-apt-get install -y build-essential gfortran libopenblas-dev liblapack-dev pkg-config
+apt-get install -y build-essential gcc g++ gfortran libopenblas-dev liblapack-dev pkg-config python3-dev
 
 # Upgrade pip to latest version
 pip install --upgrade pip setuptools wheel
 
-# Force binary-only installation for problematic packages
-pip install --only-binary=numpy,scipy numpy scipy
+# Force binary-only installation for scientific packages
+pip install --only-binary=:all: numpy scipy pandas scikit-learn
 
-# Install the rest of the requirements
-pip install -r requirements.txt
+# Install the rest of the requirements (excluding what we've already installed)
+grep -v "numpy\|scipy\|pandas\|scikit-learn" requirements.txt | pip install -r /dev/stdin
